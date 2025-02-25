578833ef2408d8ef6e77e057475d596386e761cd
Florian Grazina <Floriangrazina@gmail.com>
2025-02-25 22:57:52 +0100

    add player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,18 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed = 1f;
     private Vector2 rawInput;
 
     protected void Update()
     {
-        
+        Move();
+    }
+
+    protected void Move()
+    {
+        Vector3 delta = speed * Time.deltaTime * rawInput;
+        transform.position += delta;
     }
 
     protected void OnMove(InputValue value)
