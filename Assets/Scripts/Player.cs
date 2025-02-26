4924e51ebb65d674a9b218cad8b33a928f33a14c
Florian Grazina <Floriangrazina@gmail.com>
2025-02-26 21:03:27 +0100

    add view port camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,37 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed = 1f;
     private Vector2 rawInput;
 
+    private Vector2 minBound;
+    private Vector2 maxBound;
+
+    protected void Start()
+    {
+        InitBounds();
+    }
+
     protected void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBound = mainCamera.ViewportToWorldPoint(Vector2.zero);
+        maxBound = mainCamera.ViewportToWorldPoint(Vector2.one);
+    }
+
     protected void Move()
     {
-        Vector3 delta = speed * Time.deltaTime * rawInput;
-        transform.position += delta;
+        Vector2 delta = speed * Time.deltaTime * rawInput;
+
+        Vector2 newPos = new()
+        {
+            x = Mathf.Clamp(transform.position.x + delta.x, minBound.x, maxBound.x),
+            y = Mathf.Clamp(transform.position.y + delta.y, minBound.y, maxBound.y)
+        };
+
+        transform.position = newPos;
     }
 
     protected void OnMove(InputValue value)
