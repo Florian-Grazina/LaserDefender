c442feb7cd5f8432ca76a6504cdf9ef8bceffe7d
Florian Grazina <Floriangrazina@gmail.com>
2025-02-28 21:12:35 +0100

    enemy spawner

---
+++ Assets/Scripts/EnemySpanwer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EnemySpanwer : MonoBehaviour
+{
+    [SerializeField] private WaveConfigSO currentWave;
+
+    protected void Start()
+    {
+        SpawnEnemies();
+    }
+
+    private void SpawnEnemies()
+    {
+        Instantiate(currentWave.GetEnemyPrefab(0),
+                    currentWave.GetStartingWaypoint().position,
+                    Quaternion.identity);
+
+    }
+}
--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -6,12 +6,20 @@ using System.Linq;
 [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] private List<GameObject> enemyPrefabs;
     [SerializeField] private Transform pathPrefab;
     [SerializeField] private float moveSpeed = 5f;
+
+
+    #region public methods
+    public int GetNumberOfEnemies() => enemyPrefabs.Count;
+
+    public GameObject GetEnemyPrefab(int index) => enemyPrefabs[index];
 
     public Transform GetStartingWaypoint() => pathPrefab.GetChild(0);
 
     public List<Transform> GetWaypoints() => pathPrefab.Cast<Transform>().Select(x => x).ToList();
 
     public float GetMoveSpeed() => moveSpeed;
+    #endregion
 }
