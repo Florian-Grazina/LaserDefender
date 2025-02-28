dc30463c71d126fdf12e486e170c93f02efe8a8d
Florian Grazina <Floriangrazina@gmail.com>
2025-02-28 23:48:54 +0100

    add dmg and death

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -3,5 +3,11 @@ using UnityEngine;
 public class DamageDealer : MonoBehaviour
 {
     [SerializeField] private int damage = 1;
+
     public int GetDamage() => damage;
+
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,4 +2,21 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private int health = 3;
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out DamageDealer damageDealer))
+        {
+            damageDealer.Hit();
+            TakeDamage(damageDealer.GetDamage());
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+            Destroy(gameObject);
+    }
 }
