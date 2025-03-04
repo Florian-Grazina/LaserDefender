8e6b60291695a2a5d781461cde75628b57a5bff0
Florian Grazina <Floriangrazina@gmail.com>
2025-03-04 20:07:07 +0100

    add destruction prefab

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -3,15 +3,13 @@ using UnityEngine;
 public class DamageDealer : MonoBehaviour
 {
     [SerializeField] private int damage = 1;
-    [SerializeField] private float destroyDelay = 0.5f;
+    [SerializeField] private GameObject destroyEffectPrefab;
 
     public int GetDamage() => damage;
 
     public void Hit()
     {
-        GetComponent<CapsuleCollider2D>().enabled = false;
-        GetComponentInChildren<SpriteRenderer>().enabled = false;
-        Destroy(gameObject, destroyDelay);
-        GetComponent<Animator>().SetTrigger("Destroy");
+        Instantiate(destroyEffectPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
