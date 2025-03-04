fa8f66e1092ae15a0e516158155cc2bd06062fbe
Florian Grazina <Floriangrazina@gmail.com>
2025-03-04 19:50:41 +0100

    add damage animation

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class DamageDealer : MonoBehaviour
 {
     [SerializeField] private int damage = 1;
+    [SerializeField] private float destroyDelay = 0.5f;
 
     public int GetDamage() => damage;
 
     public void Hit()
     {
-        Destroy(gameObject);
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        GetComponentInChildren<SpriteRenderer>().enabled = false;
+        Destroy(gameObject, destroyDelay);
+        GetComponent<Animator>().SetTrigger("Destroy");
     }
 }
