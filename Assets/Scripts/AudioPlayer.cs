a6a37d3e0e04b3a5e96664f7b40d2f07accdc0db
Florian Grazina <Floriangrazina@gmail.com>
2025-03-20 23:23:35 +0100

    add impact sound

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -6,9 +6,19 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] private AudioClip shootingClip;
     [SerializeField][Range(0f, 1f)] private float shootingVolume = 1f;
 
+    [Header("Damage")]
+    [SerializeField] private AudioClip damageClip;
+    [SerializeField][Range(0f, 1f)] private float damageVolume = 1f;
+
     public void PlayShootingClip()
     {
         if (shootingClip != null)
             AudioSource.PlayClipAtPoint(shootingClip, Camera.main.transform.position, shootingVolume);
+    }
+
+    public void PlayDamageClip()
+    {
+        if (damageClip != null)
+            AudioSource.PlayClipAtPoint(damageClip, Camera.main.transform.position, damageVolume);
     }
 }
