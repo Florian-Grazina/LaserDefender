17a2ba9bfb360db672841967d8872e8c3a3ccfd1
Florian Grazina <Floriangrazina@gmail.com>
2025-03-23 19:41:29 +0100

    remove pitch

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -5,7 +5,6 @@ public class AudioPlayer : MonoBehaviour
     [Header("Shooting")]
     [SerializeField] private AudioClip shootingClip;
     [SerializeField][Range(0f, 1f)] private float shootingVolume = 1f;
-    [SerializeField][Range(0.1f, 3f)] private float shootingPitch = 1f;
 
     [Header("Damage")]
     [SerializeField] private AudioClip damageClip;
@@ -14,7 +13,7 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField][Range(0f, 1f)] private float deathVolume = 1f;
     public void PlayShootingClip()
     {
-        PlayClip(shootingClip, shootingVolume, shootingPitch);
+        PlayClip(shootingClip, shootingVolume);
     }
 
     public void PlayDamageClip()
@@ -31,19 +30,5 @@ public class AudioPlayer : MonoBehaviour
     {
         if (clip != null)
             AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position, volume);
-    }
-
-    private void PlayClip(AudioClip clip, float volume, float pitch)
-    {
-        if (clip != null)
-        {
-            GameObject tempAudioSource = new GameObject("TempAudio"); // Temporary object
-            AudioSource audioSource = tempAudioSource.AddComponent<AudioSource>();
-            audioSource.clip = clip;
-            audioSource.volume = volume;
-            audioSource.pitch = pitch; // Set the pitch
-            audioSource.Play();
-            Destroy(tempAudioSource, clip.length / pitch); // Destroy object after clip finishes
-        }
     }
 }
